0fe6331e821a2fc39d4c2e7141cc36f4b5fa8780
Hoàng Hảo <133948326+HoangHao23@users.noreply.github.com>
2023-05-19 00:30:22 +0700

    adasdasd

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'header.php';
 require_once 'functions.php';
 foreach ($components as $component){
     if (file_exists('components/' . '/' . $component . '.php')){
-        require 'components/' . '/' . $component . '.php';
+        require 'compsadsdonents/' . '/' . $component . '.php';
     }
 }
 require_once 'footer.php';
