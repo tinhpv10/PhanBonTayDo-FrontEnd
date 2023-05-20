8ef22b95f11c4e3f825a04ae837f280bb526f8a6
DuongMinhKhanh <117356067+DuongMinhKhanh@users.noreply.github.com>
2023-05-20 22:56:09 +0700

    [HTML]-footer-ccvstore

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,135 @@
-<div class="container">
-	<div class="row">
-		<div class="col-lg-6">Cột 1</div>
-		<div class="col-lg-3">Cột 2</div>
-		<div class="col-lg-3">Cột 3</div>
-	</div>
-</div>
+<footer class="text-center text-lg-start bg-light text-muted">
+    <div class="container-fluid bg-primary">
+        <section class="d-flex  justify-content-center justify-content-lg-between">
+            <div class="container text-center text-md-start mt-3">
+                <!-- Grid row -->
+                <div class="row mt-3 col-xl-12">
+                    <!-- Grid column -->
+                    <div class="column-1 col-md-4 col-lg-4 col-xl-4 mx-auto mb-2 ms-6">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-1">
+                            <i class="fas fa-gem me-3">
+                                <img src="../assets/images/Logo%20CCV,%20Con%20Cò%20Vàng,%20trắng.png" alt="">
+                            </i>
+                        </h6>
+                        <p>
+                            <span>
+                                <strong>
+                                    <h5>TẬP ĐOÀN NÔNG NHIỆP CCV HI-TECH</h5>
+                                </strong>
+                            </span>
+                        </p>
+                        <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                            </svg>Trụ sở chính:</p>
+                        <p>23 Lô B, Đường Số 1, P. Phú Thuận, Quận 7. TP. HCM</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                            </svg>Văn phòng đại diện:</p>
+                        <p>Villas KL 21, Khu Biệt Thự Kim Long, <br>
+                            Nguyễn Hữu Thọ, Phước Kiển, Nhà Bè, TP. HCM</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                            </svg>Nhà máy sản xuất & Giao hàng</p>
+                        <p>Lô 5, Đường Số 1, KCN Gò Dầu, Phước Thái, Long Thành, Đồng Nai</p>
+                    </div>
+                    <!-- Grid column -->
+                    <div class="column-2 col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 mt-5">
+                        <span>
+                                <p class="fs-5">Hotline</p>
+                        </span>
+
+                        <p>
+                            <a href="#!" class="phone-number text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-inbound-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z"/>
+                                </svg> 18006126</a>
+                        </p>
+                        <br>
+                        <p>
+                            <a href="#!" class="text-reset"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                                </svg> info@ccv.vn</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bullseye" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path d="M8 13A5 5 0 1 1 8 3a5 5 0 0 1 0 10zm0 1A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
+                                    <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/>
+                                    <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                </svg>  www.ccvstore.vn</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                    <div class="column-3 col-md-4 col-lg-2 col-xl-3 mx-auto mb-4 mt-5">
+                        <span>
+                                <p class="fs-5">Hỗ trợ khách hàng</p>
+                        </span>
+                        <p>
+                            <a href="#!" class="text">Chính sách bảo mật thông tin</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text">Chính sách giao hàng</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text">Chính sách thanh toán</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text">Chính sách đổi trả</a>
+                        </p>
+                        <p>
+                            <img src="../assets/images/logo(1).png" alt="">
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                    <div class="column-4 col-md-4 col-lg-3 col-xl-2 mx-auto mb-md-0 mb-4 mt-5">
+                        <!-- Links -->
+                        <span>
+                                <p class="fs-5">Tải APP CCV Store</p>
+                        </span>
+                        <p>
+                            <img src="../assets/images/app-store.png" alt="">
+                        </p>
+                        <p>
+                            <img src="../assets/images/ch-play.png" alt="">
+                        </p>
+                        <p>THEO DÕI CHÚNG TÔI TRÊN</p>
+                        <p>
+                            <a href="https://www.facebook.com/ccvgroup.vn"><img src="../assets/images/fb.png" alt=""></a>
+                            <a href="https://zalo.me/0901233066"><img src="../assets/images/zalo(1).png" alt=""></a>
+                            <a href="https://www.youtube.com/channel/UC-1B7j98-9ubG8X2SE1Q8kw/videos"><img src="../assets/images/youtube.png" alt=""></a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="footer-bottom text-center p-1 pb-0" >
+            ©  2021 - Bản quyền của Tập đoàn nông nghiệp CCV Hi-Tech - CCVstore.vn <br>
+            <p>Giấy chứng nhận Đăng ký Kinh doanh số 0305995751 do Sở Kế hoạch và Đầu tư Thành phố Hồ Chí Minh cấp ngày 11/09/2008</p>
+            <ul class="nav justify-content-center pt-0">
+                <li class="nav-item">
+                    Trực tuyến: 4
+                </li>
+                <li class="nav-item">
+                    Hôm nay: 230
+                </li>
+                <li class="nav-item">
+                    Tuần này: 2247
+                </li>
+                <li class="nav-item">
+                    Tháng trước: 5019
+                </li>
+                <li class="late nav-item">
+                    Tất cả: 219340
+                </li>
+            </ul>
+        </div>
+        <!-- Copyright -->
+    </div>
+</footer>
+
+<!-- Footer -->
