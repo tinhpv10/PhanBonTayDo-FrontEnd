ca3a9df149e21083861e8f722a63a9453e31157b
Vwebsite <viluong04@gmail.com>
2023-05-25 15:26:47 +0700

    [HTMl]-banner-PBTD

--- components/banner.php
+++ components/banner.php
@@ -1,39 +1,35 @@
-<section class="banner-box mt-5 mb-5 d-none d-lg-block">
-	<div class="container d-flex justify-content-center ">
-		 <div class="images-banner border">
-			 <div class="content-box">
-				 <img src="<?= ASSETS ?>/images/flowers-banner-large-min.jpg" alt="flowers-banner-large-min">
-				 <div class="content text-center p-4">
-					 <h4 class="banner-title fw-bold fs-1 text-danger">Quốc Tế<br>Phụ Nữ 8-3</h4>
-					 <p>Là ngày chúng ta thể hiện sự chăm sóc yêu thương cho người phụ nữ mà mình yêu quý.</p>
-					 <button class="btn btn-success rounded-5">TO SHOP</button>
-				 </div>
-			 </div>
-		 </div>
-		<div class="images-banner ms-3 me-3">
-			<div class="content-box mb-3">
-				<img src="<?= ASSETS ?>/images/flowers-banner-medium-top.jpg" alt="flowers-banner-medium-top">
-				<div class="content border-0 pt-5 ps-2">
-					<h4 class="banner-title fw-bold fs-2 text-purple">Phụ Nữ <br> Việt Nam 8-3</h4>
-					<p>Hoa tinh tế cho cô ấy.</p>
-				</div>
-			</div>
-			<div class="content-box">
-				<img src="<?= ASSETS ?>/images/flowers-banner-medium-bottom.jpg" alt="flowers-banner-medium-bottom">
-				<div class="content border-0 pt-5 ps-2">
-					<h4 class="banner-title fw-bold fs-2 text-success">Nhà Giáo <br> Việt Nam 20-11</h4>
-					<p>Gửi lời tri ân đến Thầy Cô!</p>
-				</div>
-			</div>
-		</div>
-		<div class="images-banner">
-			<div class="content-box">
-				<img src="<?= ASSETS ?>/images/flowers-bottom-right-full-heigh-min.jpg" alt="flowers-bottom-right-full-heigh-min">
-				<div class="content text-center pt-3">
-					<h4 class="banner-title fw-bold fs-1 text-light-blue">Hoa <br> Sinh Nhật</h4>
-					<p>Món quà tinh tế trong ngày sinh nhật.</p>
-				</div>
-			</div>
-		</div>
-	</div>
+<section class="banner-box mt-5 mb-5 d-lg-block">
+    <div class="container d-flex justify-content-center">
+
+        <div class="images-banner border">
+            <div class="content-box col-lg-8">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1200">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="<?= ASSETS ?>/images/PropertyDefault.jpg"   alt="flowers-banner-large-min">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="<?= ASSETS ?>/images/PropertyVariant2.jpg"  alt="flowers-banner-large-min">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="<?= ASSETS ?>/images/PropertyVariant3.jpg" alt="flowers-banner-large-min">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="images-banner ms-3 me-3 col-lg-4">
+            <div class="content-box-img mb-3">
+                <img src="<?= ASSETS ?>/images/PropertyVariant3.jpg" class="d-block w-100" alt="flowers-banner-medium-top">
+            </div>
+            <div class="content-box-img">
+                <img src="<?= ASSETS ?>/images/PropertyVariant3.jpg" class="d-block w-100" alt="flowers-banner-medium-bottom">
+            </div>
+        </div>
+
+
+    </div>
 </section>
--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,223 @@
-<div class="container">
-	<div class="row">
-		<div class="col-lg-6">Cột 1</div>
-		<div class="col-lg-3">Cột 2</div>
-		<div class="col-lg-3">Cột 3</div>
-	</div>
-</div>
+<footer>
+
+    <!-- Section: Links  -->
+    <section class="information justify-content-center justify-content-lg-between p-4">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row">
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4 light">
+                        Về chúng tôi
+                    </h6>
+                    <span></span>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/gioi-thieu/" class="text-reset">Thông tin</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/lien-he/" class="text-reset">Liên hệ</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/" class="text-reset">Chứng nhận</a>
+                    </div>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4 light">
+                        Trợ giúp
+                    </h6>
+                    <span></span>
+
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/" class="text-reset">Câu hỏi thường gặp</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/blog/" class="text-reset">Tin tức</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/" class="text-reset">Trả hàng</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/thanh-toan-va-van-chuyen/" class="text-reset">Thanh toán và vận chuyển</a>
+                    </div>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4 light">
+                        Sản phẩm
+                    </h6>
+                    <span></span>
+
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/lop-hoc-trong-lan/" class="text-reset">Lớp học trồng Lan</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/danh-muc/thuoc-bvtv/" class="text-reset">Thuốc bảo vệ thực vật</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/danh-muc/thuoc-bvtv/thuoc-tru-benh-cay/" class="text-reset">Thuốc trị nấm</a>
+                    </div>
+                    <div class="about">
+                        <a href="https://nongnghiepdep.com/danh-muc/phan-bon-cay-trong/" class="text-reset">Phân bón cây trồng</a>
+                    </div>
+                </div>
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-3 hotline">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4 light">Hotline</h6>
+                    <span></span>
+
+                    <div class="phone">
+                        <a href="tel:0947399439" class="text-reset">
+                            <img src="assets/images/phone-call.png" alt=""><span>0947.399.439</span>
+                        </a>
+                    </div>
+                    <div class="phone">
+                        <a href="tel:0354030468" class="text-reset">
+                            <img src="assets/images/phone-call.png" alt=""><span>0354.030.468</span>
+                        </a>
+                    </div>
+                    <div class="phone">
+                        <a href="tel:0835294953" class="text-reset">
+                            <img src="assets/images/phone-call.png" alt=""><span>0835.294.953</span>
+                        </a>
+                    </div>
+                    <div class="phone">
+                        <a href="tel:0862709750" class="text-reset">
+                            <img src="assets/images/phone-call.png" alt=""><span>0862.709.750</span>
+                        </a>
+                    </div>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-3">
+                    <h6 class="text-uppercase fw-bold mb-4 light">Kết nối mạng xã hội</h6>
+                    <span></span>
+
+                    <!-- Section: Social media -->
+                    <section class="mb-4 icon">
+                        <!-- Facebook -->
+                        <a
+                                class="btn text-white btn-floating"
+                                style="background-color: #3b5998;"
+                                href="https://www.facebook.com/shopnongnghiepdep/"
+                                role="button"
+                        ><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                            </svg></a>
+                        <!-- instagram -->
+                        <a
+                                class="btn text-white btn-floating"
+                                style="background-color: #456990;"
+                                href="#!"
+                                role="button"
+                        ><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                            </svg></a>
+
+                        <!-- Twitter -->
+                        <a
+                                class="btn text-white btn-floating"
+                                style="background-color: #3c77b5;"
+                                href="#!"
+                                role="button"
+                        ><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                            </svg></a>
+
+
+                        <!-- thư -->
+                        <a
+                                class="btn text-white btn-floating"
+                                style="background-color: black;"
+                                href="nam.nongnghiepdep@gmail.com"
+                                role="button"
+                        ><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-mail" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                            </svg></a>
+
+                        <!-- phone-->
+                        <a
+                                class="btn text-white btn-floating"
+                                style="background-color: #0082ca;"
+                                href="tel:0862709750"
+                                role="button"
+                        ><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-phone-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                            </svg></a>
+                        <!-- youtube -->
+                        <a
+                                class="btn text-white btn-floating"
+                                style="background-color: red;"
+                                href="https://www.youtube.com/nongnghiepdep"
+                                role="button"
+                        ><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+                                <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                            </svg></a>
+                    </section>
+                    <!-- Section: Social media -->
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+    <hr>
+
+    <section class="d-flex" style="margin-top: 30px; margin-bottom: 50px;">
+        <div class="container">
+            <div class="row d-flex flex-row">
+                <a href="https://www.lazada.vn/shop/agribe" class="col-lg-3 d-flex align-items-center">
+                    <img src="assets/images/68a3c135bd49ae699e90a4da955b7ffb.png" alt="" class="small">
+                    <div class="ml-2 bold">
+                        Gian hàng <br>
+                        Nông nghiệp Lazada
+                    </div>
+                </a>
+                <a href="https://shopee.vn/phanthuoc24h" class="col-lg-3 d-flex align-items-center">
+                    <img src="assets/images/526e7194884d4a9141dbd637d89b202b.png" alt="" class="small">
+                    <div class="ml-2 bold">
+                        Gian hàng <br>
+                        Nông nghiệp Shopee
+                    </div>
+                </a>
+
+                <div class="col-lg-6 text-end text">
+                    <a href="https://nongnghiepdep.com/">Điều khoản sử dụng</a> -
+                    <a href="https://nongnghiepdep.com/">Chính sách bảo mật</a>
+                </div>
+            </div>
+
+            <!-- <div class="row">
+                <div class="col-lg-6 d-flex align-items-center">
+                    <a href="">Điều khoản sử dụng</a> -
+                    <a href="">Chính sách bảo mật</a>
+                </div>
+            </div> -->
+
+
+        </div>
+    </section>
+    <div class="container-fluid color">
+        <div class="absolute-footer dark medium-text-center text-center">
+            <div class="container clearfix">
+                <div class="footer-primary pull-left">
+                    <div class="copyright-footer">
+                        2023 © Nông Nghiệp Đẹp
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
