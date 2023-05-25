9e77a440f3450467f4d708b8a98d9bbd227395c0
DuongMinhKhanh <117356067+DuongMinhKhanh@users.noreply.github.com>
2023-05-25 14:20:26 +0700

    [HTML]-footer-PBTD

--- components/footer.php
+++ components/footer.php
@@ -1,135 +1,251 @@
+<!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted">
-    <div class="container-fluid bg-primary">
-        <section class="d-flex  justify-content-center justify-content-lg-between">
-            <div class="container text-center text-md-start mt-3">
-                <!-- Grid row -->
-                <div class="row mt-3 col-xl-12">
-                    <!-- Grid column -->
-                    <div class="column-1 col-md-4 col-lg-4 col-xl-4 mx-auto mb-2 ms-6">
-                        <!-- Content -->
-                        <h6 class="text-uppercase fw-bold mb-1">
-                            <i class="fas fa-gem me-3">
-                                <img src="../assets/images/Logo%20CCV,%20Con%20Cò%20Vàng,%20trắng.png" alt="">
-                            </i>
-                        </h6>
-                        <p>
-                            <span>
-                                <strong>
-                                    <h5>TẬP ĐOÀN NÔNG NHIỆP CCV HI-TECH</h5>
-                                </strong>
-                            </span>
-                        </p>
-                        <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                            </svg>Trụ sở chính:</p>
-                        <p>23 Lô B, Đường Số 1, P. Phú Thuận, Quận 7. TP. HCM</p>
-                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                            </svg>Văn phòng đại diện:</p>
-                        <p>Villas KL 21, Khu Biệt Thự Kim Long, <br>
-                            Nguyễn Hữu Thọ, Phước Kiển, Nhà Bè, TP. HCM</p>
-                        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                            </svg>Nhà máy sản xuất & Giao hàng</p>
-                        <p>Lô 5, Đường Số 1, KCN Gò Dầu, Phước Thái, Long Thành, Đồng Nai</p>
-                    </div>
-                    <!-- Grid column -->
-                    <div class="column-2 col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 mt-5">
-                        <span>
-                                <p class="fs-5">Hotline</p>
-                        </span>
-
-                        <p>
-                            <a href="#!" class="phone-number text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-inbound-fill" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z"/>
-                                </svg> 18006126</a>
-                        </p>
-                        <br>
-                        <p>
-                            <a href="#!" class="text-reset"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-                                </svg> info@ccv.vn</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text-reset"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bullseye" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8 13A5 5 0 1 1 8 3a5 5 0 0 1 0 10zm0 1A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
-                                    <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/>
-                                    <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                                </svg>  www.ccvstore.vn</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-                    <div class="column-3 col-md-4 col-lg-2 col-xl-3 mx-auto mb-4 mt-5">
-                        <span>
-                                <p class="fs-5">Hỗ trợ khách hàng</p>
-                        </span>
-                        <p>
-                            <a href="#!" class="text">Chính sách bảo mật thông tin</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text">Chính sách giao hàng</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text">Chính sách thanh toán</a>
-                        </p>
-                        <p>
-                            <a href="#!" class="text">Chính sách đổi trả</a>
-                        </p>
-                        <p>
-                            <img src="../assets/images/logo(1).png" alt="">
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-                    <div class="column-4 col-md-4 col-lg-3 col-xl-2 mx-auto mb-md-0 mb-4 mt-5">
-                        <!-- Links -->
-                        <span>
-                                <p class="fs-5">Tải APP CCV Store</p>
-                        </span>
-                        <p>
-                            <img src="../assets/images/app-store.png" alt="">
-                        </p>
-                        <p>
-                            <img src="../assets/images/ch-play.png" alt="">
-                        </p>
-                        <p>THEO DÕI CHÚNG TÔI TRÊN</p>
-                        <p>
-                            <a href="https://www.facebook.com/ccvgroup.vn"><img src="../assets/images/fb.png" alt=""></a>
-                            <a href="https://zalo.me/0901233066"><img src="../assets/images/zalo(1).png" alt=""></a>
-                            <a href="https://www.youtube.com/channel/UC-1B7j98-9ubG8X2SE1Q8kw/videos"><img src="../assets/images/youtube.png" alt=""></a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between border-bottom email-footer-color mt-4">
+        <div class="container text-center text-md-start ">
+            <!-- Grid row -->
+            <div class="row mt-2">
+                <!-- Grid column -->
+                <div class="col-md-1 col-lg-1 col-xl-2 mx-auto">
+                    <img class="mail-image" src="../assets/images/ci_mail-open.svg" alt="">
                 </div>
-                <!-- Grid row -->
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-3 col-xl-3 mx-auto mt-2 text-uppercase">
+                    <h5>đăng ký nhận bảng tin</h5>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column-3 col-md-4 col-lg-4 col-xl-4 mx-auto mb-2">
+                    <div class="input-group">
+                        <input type="email" class="form-control" placeholder="Nhập email...">
+                        <button class="input-group-text btn" type="submit" id="basic-addon2"><i class='bx bxs-send'></i></button>
+                    </div>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column-4 col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mt-1">
+                    <div class="headphone ">
+                        <i class='bx bx-headphone bx-tada' ></i>
+                        <h5 class="mt-1">Hỗ trợ 24/7</h5>
+                    </div>
+                </div>
+                <!-- Grid column -->
             </div>
-        </section>
-        <!-- Section: Links  -->
-
-        <!-- Copyright -->
-        <div class="footer-bottom text-center p-1 pb-0" >
-            ©  2021 - Bản quyền của Tập đoàn nông nghiệp CCV Hi-Tech - CCVstore.vn <br>
-            <p>Giấy chứng nhận Đăng ký Kinh doanh số 0305995751 do Sở Kế hoạch và Đầu tư Thành phố Hồ Chí Minh cấp ngày 11/09/2008</p>
-            <ul class="nav justify-content-center pt-0">
-                <li class="nav-item">
-                    Trực tuyến: 4
-                </li>
-                <li class="nav-item">
-                    Hôm nay: 230
-                </li>
-                <li class="nav-item">
-                    Tuần này: 2247
-                </li>
-                <li class="nav-item">
-                    Tháng trước: 5019
-                </li>
-                <li class="late nav-item">
-                    Tất cả: 219340
-                </li>
-            </ul>
+            <!-- Grid row -->
         </div>
-        <!-- Copyright -->
-    </div>
-</footer>
 
+
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="section-footer">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="column col-md-2 col-lg-3 col-xl-3 mx-auto pl-1">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        thông tin
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-secondary">Chính sách bảo mật</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Chính sách giao hàng</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Chính sách đổi hàng</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Chính sách bảo hành</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Chính sách thanh toán</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column col-md-2 col-lg-3 col-xl-3 mx-auto">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Tài khoản
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-secondary">Tài khoản của tôi</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Xem giỏ hàng</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Lích sử đơn hàng</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Giao dịch của bạn</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Danh sách yêu thích</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column col-md-3 col-lg-3 col-xl-3 mx-auto">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Về chúng tôi
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-secondary">Giới thiệu</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Liên hệ với chúng tôi</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Bảo hành và dịch vụ</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Hỗ trợ trang 24/7</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Điều khoản và điều kiện</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Hỗ trợ
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-secondary">Nhãn hiệu</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Phiếu quà tặng</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Chi nhánh</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Đặc biệt</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-secondary">Trả về</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <hr class="text-secondary">
+            <!-- Grid row -->
+        </div>
+    </section>
+    <section class="">
+        <div class="container text-center text-md-start">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="column col-md-3 col-lg-3 col-xl-2 mx-auto pl-1 mb-4">
+                    <!-- Content -->
+                    <h5 class="text-uppercase fw-bold mb-3">
+                        mạng xã hội
+                    </h5>
+                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fphanbontaydocantho&tabs=timeline&width=220&height=170&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=305008981502392" width="220" height="170" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column col-md-3 col-lg-3 col-xl-4 mx-auto mb-4">
+                    <!-- Links -->
+                    <h5 class="text-uppercase fw-bold mb-3">
+                        liên hệ với chúng tôi
+                    </h5>
+                   <div class="text-secondary">
+                       <p>
+                           Địa chỉ: 161H1/13, Nguyễn Văn Cừ,P.An Khánh, Q.Ninh Kiều, TP Cần Thơ
+                       </p>
+                       <p>
+                           <a class="text-secondary" href="mailto:phanbonhuucotaydo@gmail.com">Email: phanbonhuucotaydo@gmail.com</a>
+                       </p>
+                       <p>
+                           <a class="text-secondary" href="tel:0975 533 853">Phone: 0975 533 853</a>
+                       </p>
+                   </div>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column col-md-3 col-lg-3 col-xl-3 mx-auto mb-4">
+                    <!-- Links -->
+                    <h5 class="text-uppercase fw-bold mb-3">
+                        phương thức thanh toán
+                    </h5>
+                    <div class="type-bank card-group grid gap-4 col-lx3 mb-4">
+                             <a class="card-group atm mx-0 mb-1" href="">
+                                 <img src="../assets/images/Payment%20Method/ATM.svg" class="card-img-top" alt="ATM">
+                             </a>
+                             <a class="card-group atm mx-0 mb-1" href="">
+                                 <img src="../assets/images/Payment%20Method/cod.svg" class="card-img-top" alt="COD">
+                             </a>
+                             <a class="card-group atm mx-0 mb-1" href="">
+                                 <img src="../assets/images/Payment%20Method/JCB.svg" class="card-img-top" alt="JCB">
+                             </a>
+                             <a class="card-group atm mx-0 mb-1" href="">
+                                 <img src="../assets/images/Payment%20Method/Mastercard.svg" class="card-img-top" alt="Mastercard">
+                             </a>
+                            <a class="card-group atm mx-0 mb-1" href="">
+                                <img src="../assets/images/Payment%20Method/momo.svg" class="card-img-top" alt="momo">
+                            </a>
+                            <a class="card-group atm mx-0 mb-1" href="">
+                                <img src="../assets/images/Payment%20Method/zalopay.svg" class="card-img-top" alt="zalopay">
+                            </a>
+                            <a class="card-group atm mx-0 mb-1" href="">
+                                <img src="../assets/images/Payment%20Method/vnpay.svg" class="card-img-top" alt="vnpay">
+                            </a>
+                    </div>
+
+                    <br>
+
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="column col-md-4 col-lg-3 col-xl-2 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h5 class="text-uppercase fw-bold mb-3">
+                        chứng nhận
+                    </h5>
+                    <img src="../assets/images/logo(1).png" alt="" class="online">
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="bottom-footer text-center p-3" >
+        <ul class="list-group ">
+            <li class="list-group ">TÂY ĐÔ | PHÂN BÓN  © 2018 - 2022. All Rights Reserved</li>
+            <div class="font-footer">
+                <li class="list-group">Công ty TNHH PHÂN BÓN HỮU CƠ TÂY ĐÔ</li>
+                <li class="list-group"><a class="text-reset" href="#">MST: 1801736019</a></li>
+                <li class="list-group">161H1/13, Nguyễn Văn Cừ, P. An Khánh, Q. Ninh Kiều, TP. Cần Thơ</li>
+            </div>
+        </ul>
+
+    </div>
+    <!-- Copyright -->
+</footer>
 <!-- Footer -->
