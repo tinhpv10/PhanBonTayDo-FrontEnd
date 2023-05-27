40030060501a9ff4e2dc72fcf66909ed0b37f254
Nguyen Minh Ty <99376010+minhtycode@users.noreply.github.com>
2023-05-27 18:06:25 +0700

    [HTML]-HEADER-PBTD-(desktop)

--- components/header.php
+++ components/header.php
@@ -1,17 +1,92 @@
 <header>
-
-	<nav class="primary-menu">
-		<ul>
-			<li><a href="">Danh mục sản phẩm</a>
-				<ul>
-					<li><a href="">Tôn nhựa</a></li>
-					<li><a href="">Menu 2</a></li>
-				</ul>
-			</li>
-			<li><a href="">Trang chủ</a></li>
-			<li><a href="">Giới thiệu</a></li>
-			<li><a href="">Liên hệ</a></li>
-		</ul>
-	</nav>
-
+    <div class="container">
+        <div class="d-flex flex-column ">
+            <div class="p-2">
+                <div class="">
+                    <div class="me-auto p-2 ">
+                        <div class="row top-header">
+                            <div class="col-lg-6 col-md-12 ask text-white">
+                                <a href="#" class="text-decoration-none text-white contact px-3 border-end ">Tư vấn chọn mua </a> &nbsp;
+                                <a href="#" class="text-decoration-none text-white contact px-3 border-end ">Hỏi đáp</a>
+                                <a href="#" class="text-decoration-none text-white contact px-3 ">Chúng tôi là ai?</a>
+                            </div>
+                            <div class="d-flex  justify-content-end col-lg-6 col-md-12 text-white ">
+                                <i class="bx bxs-phone-call pe-2 pt-2"></i>
+                                <div class="phone d-flex flex-column">
+                                    0975 583 853
+                                    <div>
+                                        HOTLINE
+                                    </div>
+                                </div>
+                                <i class="bx bxs-bell-ring pt-2 ps-2"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 row d-flex">
+                        <div class="col-lg-4 col-md-8 logo d-flex">
+                            <div class="logo-img">
+                                <img src="http://phanbontaydo/assets//images/logo.png" class="img" alt="">
+                            </div>
+                            <div class="name-shop align-middle text-center p-2 pt-3 text-white ">
+                                Phân bón hữu cơ Tây Đô
+                            </div>
+                        </div>
+                        <div class="col-lg-8 ">
+                            <form action="">
+                                <div class="input-group  ">
+                                    <div class="col-lg-3 ">
+                                        <select class="form-select select " aria-label="Default select example">
+                                            <option selected>Tất cả danh mục</option>
+                                            <option value="Danh Mục 1">Danh Mục 1</option>
+                                            <option value="Danh Mục 2">Danh Mục 2</option>
+                                            <option value="Danh Mục 3">Danh Mục 3 </option>
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-7 d-flex">
+                                        <input type="search" class="form-control search" placeholder="Tìm kiếm sản phẩm..." aria-label="Example text with button addon" aria-describedby="button-addon1">
+                                        <button class="btn btn-light search-group fw-bold " type="button" id="button-addon2"><i class='bx bx-search-alt-2'></i></button>
+                                    </div>
+                                    <div class="col-lg-2 register">
+                                        <a href="#" class="btn btn-light register w-100 align-middle fw-bold center-text ">ĐĂNG KÝ ĐẠI LÝ</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="p-2">
+                <nav class="navbar nav-menu navbar-expand-lg bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand text-white border-end" href="#"><i class='bx bx-category'></i> Danh Mục Sản Phẩm </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse  navbar-collapse" id="menu">
+                            <ul class="navbar-nav ">
+                                <li class="nav-item">
+                                    <a class="nav-link " aria-current="page" href="#">Phân lá bón</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Phân bón gốc</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Phân bón hữu cơ </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link ">Thuốc bảo vệ thực vật </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link ">Hoa kiểng </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link ">Khuyến mãi hôm nay <i class='bx bxs-hot'></i>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
 </header>
